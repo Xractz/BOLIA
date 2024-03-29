3a9c0591491e620d34a11e17699e96278e63e4bc
samchristg <48348791+Xractz@users.noreply.github.com>
2024-03-29 23:42:40 +0700

    ✨ feat: add handlers
    Add room and time handlers.

--- src/config/history.js
+++ src/config/history.js
@@ -1,11 +1,15 @@
 const Home = require("../Actions/Message/home");
 const Menu = require("../Actions/Message/menu");
 const Book = require("../Actions/Message/book");
+const Room = require("../Actions/Message/room");
+const Time = require("../Actions/Message/time");
 
 const historyHandler = {
   home: Home.execute,
   menu: Menu.execute,
-  book: Book.execute
+  book: Book.execute,
+  room: Room.execute,
+  time: Time.execute,
 };
 
 class Handlers {

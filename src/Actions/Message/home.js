1b650bd01b81d0640ca08b561e16fa193e27dc41
samchristg <48348791+Xractz@users.noreply.github.com>
2024-03-29 20:42:30 +0700

    ♻️ refactor: Reorganize home action
    The `home` action has been reorganized and refactored to improve its structure and maintainability. The code has been redesigned to separate out the logic for checking the user's status and retrieving their name from the database. Additionally, the code has been made more efficient by reducing the number of database queries.

--- src/Actions/Message/home.js
+++ src/Actions/Message/home.js
@@ -8,15 +8,12 @@ const {
 class Home {
   async execute(sock, message) {
     try {
-      if (await onDB(getPhoneNumber(message))) {
+      const status = await onDB(getPhoneNumber(message));
+      
+      if (status) {
         const name = await checkData(getPhoneNumber(message), "name");
-        await sendMessageWTyping(sock, getJid(message), {
-          text: `Halo, ${name}\n\nSelamat datang di *BOLIA*\nUnofficai BOT Library UAJY\n\nDisini kamu bisa melihat ruang yang tersedia dan memesannya 🤗\n\nSilahkan memilih menu di bawah ini :\n\n\t\`1\` Ketersediaan ruang\n\t\`2\` Pesan ruang`,
-        });
-      } else {
-        await sendMessageWTyping(sock, getJid(message), {
-          text: "Selamat datang di *BOLIA*\nUnofficai BOT Library UAJY\n\nDisini kamu bisa melihat ruang yang tersedia dan memesannya 🤗\n\nSilahkan memilih menu di bawah ini :\n\n\t`1` Ketersediaan ruang\n\t`2` Pesan ruang",
-        });
+        const text = status ? `Halo, ${name}\n\nSelamat datang kembali di *BOLIA*\nUnofficai BOT Library UAJY\n\nSilahkan memilih menu di bawah ini :\n\n\t\`1\` Ketersediaan ruang\n\t\`2\` Pesan ruang` : "Selamat datang di *BOLIA*\nUnofficai BOT Library UAJY\n\nDisini kamu bisa melihat ruang yang tersedia dan memesannya 🤗\n\nSilahkan memilih menu di bawah ini :\n\n\t`1` Ketersediaan ruang\n\t`2` Pesan ruang";
+        await sendMessageWTyping(sock, getJid(message), { text });
       }
       
       await updateData(getPhoneNumber(message), { status: "online", history: 'menu' });

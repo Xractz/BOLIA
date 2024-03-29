c1c7c1a70f10add94204196b3f155e2243d28b98
samchristg <48348791+Xractz@users.noreply.github.com>
2024-03-29 23:45:19 +0700

    ♻️ refactor: Separate name variable
    Separate name as a variable

--- src/Actions/Message/home.js
+++ src/Actions/Message/home.js
@@ -2,26 +2,28 @@ const { updateData, onDB, checkData } = require("../../supports/database");
 const { 
   sendMessageWTyping, 
   getPhoneNumber, 
-  getJid
+  getJid 
 } = require("../../supports/message");
-
+var name;
 class Home {
   async execute(sock, message) {
     try {
       const status = await onDB(getPhoneNumber(message));
-      
-      if (status) {
-        const name = await checkData(getPhoneNumber(message), "name");
-        const text = status ? `Halo, ${name}\n\nSelamat datang kembali di *BOLIA*\nUnofficai BOT Library UAJY\n\nSilahkan memilih menu di bawah ini :\n\n\t\`1\` Ketersediaan ruang\n\t\`2\` Pesan ruang` : "Selamat datang di *BOLIA*\nUnofficai BOT Library UAJY\n\nDisini kamu bisa melihat ruang yang tersedia dan memesannya 🤗\n\nSilahkan memilih menu di bawah ini :\n\n\t`1` Ketersediaan ruang\n\t`2` Pesan ruang";
-        await sendMessageWTyping(sock, getJid(message), { text });
+
+      if (status)
+      {
+        name = await checkData(getPhoneNumber(message), "name");
       }
-      
-      await updateData(getPhoneNumber(message), { status: "online", history: 'menu' });
+
+      const text = status
+        ? `Halo, ${name}\n\nSelamat datang kembali di *BOLIA*\nUnoffical BOT Library UAJY\n\nSilahkan memilih menu di bawah ini :\n\n\t\`1\` Ketersediaan ruang\n\t\`2\` Pesan ruang`
+        : "Selamat datang di *BOLIA*\nnUnoffical BOT Library UAJY\n\nDisini kamu bisa melihat ruang yang tersedia dan memesannya 🤗\n\nSilahkan memilih menu di bawah ini :\n\n\t`1` Ketersediaan ruang\n\t`2` Pesan ruang";
+      await sendMessageWTyping(sock, getJid(message), { text });
+      await updateData(getPhoneNumber(message), { status: "online", history: "menu" });
     } catch (error) {
       console.error(error);
     }
   }
-
 }
 
 module.exports = new Home();

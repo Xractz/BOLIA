cdea1bfc2a6f946f2e452d0ba68cd3c9e3ec4cea
samchristg <48348791+Xractz@users.noreply.github.com>
2024-04-03 11:01:42 +0700

    ♻️ refactor(supports): refactoring temp package to using class

--- src/supports/temp.js
+++ src/supports/temp.js
@@ -1,17 +1,90 @@
-let temp = [];
-let result = null
+const fs = require("fs").promises;
 
-const setTempData = async (data) => {
-  temp.push(data);
+class Temp {
+  constructor() {
+    this.filename = "./src/Temp/temp.json";
+  }
+
+  async readData() {
+    try {
+      const data = await fs.readFile(this.filename, "utf8");
+      return JSON.parse(data);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async createData(jsonData) {
+    try {
+      const fileExists = await fs
+        .access(this.filename)
+        .then(() => true)
+        .catch(() => false);
+      if (!fileExists) {
+        await fs.writeFile(this.filename, "[]", "utf8");
+      }
+
+      let data = [];
+      const existingData = await fs.readFile(this.filename, "utf8");
+      data = JSON.parse(existingData);
+      jsonData.listRoom = jsonData.listRoom !== undefined ? jsonData.listRoom : "";
+      jsonData.listTime = jsonData.listTime !== undefined ? jsonData.listTime : "";
+      data.push(jsonData);
+      await fs.writeFile(this.filename, JSON.stringify(data, null, 2), "utf8");
+      return true;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async readList(phoneNumber) {
+    try {
+      const jsonData = await this.readData();
+      const data = jsonData.filter((entry) => entry.phoneNumber === phoneNumber);
+      return data[0];
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async updateData(phoneNumber, { rooms, listRoom, listTime }) {
+    try {
+      let jsonData = await this.readData();
+      let modified = false;
+      jsonData.forEach((entry) => {
+        if (entry.phoneNumber === phoneNumber) {
+          entry.rooms = rooms !== undefined ? rooms : entry.rooms;
+          entry.listRoom = listRoom !== undefined ? listRoom : entry.listRoom;
+          entry.listTime = listTime !== undefined ? listTime : entry.listTime;
+          modified = true;
+        }
+      });
+
+      if (modified) {
+        await fs.writeFile(this.filename, JSON.stringify(jsonData, null, 2), "utf8");
+        return true;
+      }
+      return false;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async deleteData(phoneNumber) {
+    try {
+      let jsonData = await this.readData();
+      const filteredData = jsonData.filter((entry) => entry.phoneNumber !== phoneNumber);
+      await fs.writeFile(this.filename, JSON.stringify(filteredData, null, 4), "utf8");
+      return true;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
 }
 
-const getTempData = (phoneNumber) => {
-  const result = temp.find((data) => data.phoneNumber === phoneNumber);
-  return result ? { room: result.room, time: result.time } : null;
-};
-
-const deleteTempData = (phoneNumber) => {
-  temp = temp.filter((data) => data.phoneNumber !== phoneNumber);
-};
-
-module.exports = { setTempData, getTempData, deleteTempData };
+module.exports = new Temp();

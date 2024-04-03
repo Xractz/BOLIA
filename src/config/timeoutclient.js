54cfd136a35b2f48877ec94f7729a47058ee5824
samchristg <48348791+Xractz@users.noreply.github.com>
2024-04-03 11:02:39 +0700

    🔧 chore: deleted temp of number

--- src/config/timeoutclient.js
+++ src/config/timeoutclient.js
@@ -1,3 +1,4 @@
+const Temp = require("../supports/temp")
 const { sendMessageWTyping } = require("../supports/message");
 const { readData, updateData } = require("../supports/database");
 
@@ -8,6 +9,7 @@ class TimeoutClient {
     data.forEach(async (item) => {
       if (now >= item.timer && item.status === "online")
       {
+        await Temp.deleteData(item.phoneNumber);
         await updateData(item.phoneNumber, { status: "offline", history: "home", timer: 0, room: "", date: "", time: ""});
         await sendMessageWTyping(sock, `${item.phoneNumber}@s.whatsapp.net`, {
           text: "Yahh.. dah 5 menit loh km anggurin aku😓😓\n\nBOLIA akhiri sesi yaa..\nSilahkan chat apapun untuk memulai sesi baru🤗\n\n_Terimakasih sudah menggunakan *BOLIA* 😽_\n\nGoodbye... 👋"

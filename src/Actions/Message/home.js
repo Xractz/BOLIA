2adaea74a99ef14c3e3d4f93f652ab3befbcc1be
samchristg <48348791+Xractz@users.noreply.github.com>
2024-04-06 16:14:44 +0700

    ✨ feat: add coming soon turnitin feature
    Adds a new "Turnitin" option to the main menu.
    This feature is currently under development but will allow users to access the Turnitin plagiarism checker from within the bot.

--- src/Actions/Message/home.js
+++ src/Actions/Message/home.js
@@ -11,8 +11,8 @@ class Home {
       const name = status ? await checkData(getPhoneNumber(message), "name") : "";
 
       const text = status
-        ? `Halo, ${name}\n\nSelamat datang kembali di *BOLIA*\nUnoffical BOT Library UAJY\n\nSilahkan memilih menu di bawah ini :\n\n\t\`1\` Pesan ruang\n\n> Untuk keluar dari menu, ketik *0* atau *exit*`
-        : "Selamat datang di *BOLIA*\nUnoffical BOT Library UAJY\n\nDisini kamu bisa melihat ruang yang tersedia dan memesannya 🤗\n\nSilahkan memilih menu di bawah ini :\n\n\t`1` Pesan ruang\n\n> Untuk keluar dari menu, ketik *0* atau *exit*";
+        ? `Halo, ${name}\n\nSelamat datang kembali di *BOLIA*\nUnoffical BOT Library UAJY\n\nSilahkan memilih menu di bawah ini :\n\n\t\`1\` Pesan ruang\n\t\`2\` Turnitin (soon)\n\n> Untuk keluar dari menu, ketik *0* atau *exit*`
+        : "Selamat datang di *BOLIA*\nUnoffical BOT Library UAJY\n\nDisini kamu bisa melihat ruang yang tersedia dan memesannya 🤗\n\nSilahkan memilih menu di bawah ini :\n\n\t`1` Pesan ruang\n\t`2` Turnitin (soon)\n\n> Untuk keluar dari menu, ketik *0* atau *exit*";
       await sendMessageWTyping(sock, getJid(message), { text });
       await updateData(getPhoneNumber(message), { status: "online", history: "menu" });
     } catch (error) {
